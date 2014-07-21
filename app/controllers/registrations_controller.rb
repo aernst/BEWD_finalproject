6e9cd3e39c9095ee6482b1cfddbf71b1e7c98723
Ernst <ernsta@28cfe91ab2bd.ant.amazon.com>
2014-07-20 18:25:54 -0700

    add the viewport meta tag for mobile optimization

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,16 @@ class RegistrationsController < Devise::RegistrationsController
     summary_path
   end
 
-    def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(resource)
     summary_path
   end
+
+  def after_inactive_sign_up_path_for(resource)
+     summary_path
+  end
+
+  def after_update_path_for(resource)
+    summary_path
+  end
+  
 end
