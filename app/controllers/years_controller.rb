2f2612e77596f95e663d375511933efa3ce006ae
Ernst <ernsta@28cfe91ab2bd.ant.amazon.com>
2014-07-20 17:23:44 -0700

    added favicon and adj stylesheets

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -1,5 +1,6 @@
 class YearsController < ApplicationController
   before_action :set_year, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /years
   # GET /years.json
